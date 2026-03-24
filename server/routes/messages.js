0545237c42a1b39c11cc12e98caef3c0f58aba7a
Naagharjun <155892981+Naagharjun@users.noreply.github.com>
2026-03-24 22:42:17 +0530

    Update messages.js

--- server/routes/messages.js
+++ server/routes/messages.js
@@ -7,9 +7,10 @@ const path = require('path');
 const fs = require('fs');
 
 // Ensure uploads directory exists
-const uploadDir = 'uploads/';
+const uploadDir = '/tmp/uploads';
+
 if (!fs.existsSync(uploadDir)) {
-    fs.mkdirSync(uploadDir);
+    fs.mkdirSync(uploadDir, { recursive: true });
 }
 
 const storage = multer.diskStorage({
